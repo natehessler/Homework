bd229a1b7d5b848d648a23f44b286813c77f3afc
Nate Hessler <natehessler@gmail.com>
2022-06-20 11:24:52 -0500

    changes

--- Week2Homework.js
+++ Week2Homework.js
@@ -1,7 +1,8 @@
-let pizzaPlace = "Nate's Pizza"
-let numberOfToppings = 12
-console.log(typeof pizzaPlace)
-console.log(typeof numberOfToppings)
-let greeting = "Welcome to ${pizzaPlace} where we offer ${numberoftoppings} toppings!"
-if (numberOfToppings > 10)
-  console.log("A whole lot of pizza.")
+var pizzaPlace = "Nate's Pizza"
+var numberOfToppings = 12
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+let greeting = 'Welcome to ${pizzaPlace} where we offer ${numberoftoppings} toppings!';
+if (numberOfToppings > 10) {
+  console.log("A whole lot of pizza.");
+}
