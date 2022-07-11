125bcbfecf4f9ac9ab9ecebd3b7b7fa0fe74306c
Nate Hessler <natehessler@gmail.com>
2022-07-11 15:03:52 -0500

    week5

---
+++ classesPractice.js
@@ -0,0 +1,22 @@
+class Item {
+  constructor(name,id,price,location,description){
+      this.name = product1;
+      this.id = 1234;
+      this.price = 100;
+      this.location = "USA";
+      this.description = "Test Item";
+  }
+
+};
+
+
+
+class GameConsole {
+  constructor(brand,hardware,price,exclusives){
+    this.brand;
+    this.hardware;
+    this.price;
+    this.exclusives;
+  }
+}
+
